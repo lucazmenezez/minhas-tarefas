f4251741e13034df2094e59f742959692e022fb4
lucas menezes <menezeslucas24082002@gmail.com>
2024-11-05 10:40:59 -0300

    Criando a navegação entre rotas com Link to

---
+++ src/components/BotaoAdicionar/index.tsx
@@ -0,0 +1,5 @@
+import { EstiloBotao } from './styles'
+
+const BotaoAdicionar = () => <EstiloBotao to="/novo">+</EstiloBotao>
+
+export default BotaoAdicionar
---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const EstiloBotao = styled(Link)`
+  background-color: #44bd32;
+  color: #fff;
+  height: 64px;
+  width: 64px;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  border-radius: 50%;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+`
